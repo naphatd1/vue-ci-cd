8c55f90cb8d1ae1c14609e952927b80e64ce5e2e
Naphat <naphat.d@gmail.com>
2023-10-15 07:56:09 +0700

    v6.0.0

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Vue App v5.0.0" />
+      <HelloWorld msg="Vue App v6.0.0" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
